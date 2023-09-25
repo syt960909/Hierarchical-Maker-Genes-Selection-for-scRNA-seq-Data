5dfe0290f058bc5d43d1521614b9f1d6756e77e3
Sun <ysun465@gatech.edu>
2023-09-25 14:14:52 -0400

    updated

--- Generate_compact_heatmap.R
+++ Generate_compact_heatmap.R
@@ -36,7 +36,7 @@ for(my_level_ele in my_levels){
     for (ele in levels(pbmc_3k_hie_list[[i]])){
       
       if (my_level_ele %in% str_split(ele,';')[[1]]){
-        print(str_split(ele,';')[[1]])
+        
         route<-c(route,i)
         }
     }
@@ -49,14 +49,11 @@ over_lap<-list()
 for (i in seq_along(1:length(branch_list)-1)){
   print(i)
   over_lap<-append(over_lap,as.numeric(length(intersect(branch_list[[1]],branch_list[[i+1]]))))
-  print(intersect(branch_list[[1]],branch_list[[i+1]]))
+  
 }
 names(over_lap)<-my_levels[2:length(my_levels)]
 over_lap_s<-over_lap[order(unlist(over_lap),decreasing=TRUE)]
 my_levels[2:length(my_levels)]<-names(over_lap_s)
-for(my_level_ele in my_levels){
-  print(my_level_ele)
-}
 
 
 # Re-level object@ident
@@ -80,22 +77,41 @@ while (i<length(pbmc_3k_hie_list)){
   for (bar_code in colnames(pbmc@assays$RNA@counts)) {
     data_tmp[bar_code,]<-a$data[bar_code,]
   }
-  p_tmp=Heatmap(t(as.matrix(data_tmp)), name = paste0("mat_",as.character(i+1)), 
+  p_tmp=Heatmap(t(as.matrix(data_tmp)), #name = paste0("mat_",as.character(i+1)), 
              column_order = cell_order, 
-             row_order = row_order, 
+             row_order = rev(a$feature.order), 
              cluster_rows = FALSE,
-             cluster_columns = FALSE,show_column_names = FALSE,col = PurpleAndYellow())
+             cluster_columns = FALSE,show_column_names = FALSE,col = PurpleAndYellow(),
+             use_raster = TRUE)
   ht_list = ht_list %v% p_tmp
+  for (level_ele in levels(pbmc_3k_hie_list[[i+1]])){
+    if(length(str_split(level_ele,';')[[1]])==2){
+      pbmc_sub1 <- subset(x = pbmc, idents = str_split(level_ele,';')[[1]])
+      pbmc_sub1 <- ScaleData(pbmc_sub1, features = rownames(pbmc_3k_hie_list[[1]]))
+      pbmc.markers <- FindAllMarkers(pbmc_sub1, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+      top10 <- pbmc.markers %>% group_by(cluster) %>% top_n(n = 10, wt = avg_log2FC)
+      a<-DoHeatmap_matrix(pbmc_sub1,features = top10$gene)
+      data_tmp <- data.frame(matrix(0,nrow = 2638, ncol = ncol(a$data)))
+      colnames(data_tmp)<-colnames(a$data)
+      rownames(data_tmp)<-colnames(pbmc@assays$RNA@counts)
+      
+      for (bar_code in colnames(pbmc@assays$RNA@counts)) {
+        data_tmp[bar_code,]<-a$data[bar_code,]
+      }
+      p_tmp=Heatmap(t(as.matrix(data_tmp)), 
+                    column_order = cell_order, 
+                    row_order = rev(a$feature.order), 
+                    cluster_rows = FALSE,
+                    cluster_columns = FALSE,show_column_names = FALSE,col = PurpleAndYellow(),
+                    use_raster = TRUE)
+      ht_list = ht_list %v% p_tmp
+    }
+  }
+  
   i=i+1
 }
 
-
-
-
-ht_list = col_anno %v% p2 %v% p3 %v% p4 %v% p5 %v% p6 %v% p7 %v% p8 %v% p9
-
-draw(ht_list,main_heatmap="mat_2")
-
+draw(ht_list)
 
 
 
